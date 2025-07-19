e33442db91c9755c5d2f39e007a5ef8417a9108f
ArathySreekumar-DA <arathysreekumar2000@gmail.com>
2025-07-20 00:54:40 +0530

    Update 04_data_cleaning.sql

--- sql/04_data_cleaning.sql
+++ sql/04_data_cleaning.sql
@@ -4,7 +4,7 @@ SET Year = NULL
 WHERE Year NOT REGEXP '^(19[7-9][0-9]|20[0-1][0-9]|202[0-5])$';
 
 -- Optional: Convert 'Year' column to INT if needed
--- ALTER TABLE vgsales MODIFY COLUMN Year INT;
+ ALTER TABLE vgsales MODIFY COLUMN Year INT;
 
 -- Convert invalid 'Publisher' values (e.g., 'N/A') to NULL
 UPDATE vgsales
